b81398c64a517aab228a3f4f7f24200fc6168c14
HITO <oumar2000mouhamed@gmail.com>
2022-09-21 04:22:47 +0300

    update icon fetch API and enhance code

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,6 @@
 const body = document.body;
 const screen = document.getElementById("screen");
-const navLinks = document.querySelectorAll("[data-nav-links]");
+const navLinks = document.querySelectorAll(".nav-link");
 const searchBar = document.querySelector("[data-search-bar]");
 function load_screen(path) {
   if (path == "" || typeof path != "string") return;
@@ -14,42 +14,38 @@ function load_screen(path) {
     });
 }
 
-navLinks.forEach((navLink) => pushSVGIcon(navLink, navLink.id));
-
-function pushSVGIcon(parent, svgName, position = "before", wraper = true) {
-  // check if user passed paren and svgName arguments
-  if (arguments.length < 2)
-    return console.error(new Error("arguments are not complete"));
-
-    // fetch API require img name and go grap the image 
-  fetch("imgs/icons/nav-icons/" + svgName + ".svg")
+// fetch for icons
+function fetchIcon(svgName) {
+  if (svgName === "" || svgName == undefined)
+    return console.error(new Error("pass svg name"));
+  return fetch("imgs/icons/" + svgName + ".svg")
     .then((res) => res.text())
     .then((text) => {
       // remove live-server injection
-      // this code must be remove when uploaded on github or any other server
       if (text.indexOf("<!-- Code injected by live-server -->") !== -1) {
         text =
           text.split("<!-- Code injected by live-server -->")[0] + "</svg>";
       }
       return text;
     })
-    .then((result) => {
-      // parse an svg from the plain text result
-      let svg = new DOMParser().parseFromString(result, "text/html");
-      svg = svg.body.firstChild;
-      // append svg to the DOM with or without a wraper element
-      if (wraper === true) {
-        const div = document.createElement("div");
-        div.className = "icon-holder";
-        div.appendChild(svg);
-        if (position === "before") parent.prepend(div);
-        else if (position === "after") parent.appendChild(div);
-      } else if (wraper === false) {
-        if (position === "before") parent.prepend(svg);
-        else if (position === "after") parent.appendChild(svg);
-      }
-    })
+    .then(
+      (result) =>
+        // parse an svg from the plain text result
+        new DOMParser().parseFromString(result, "text/html").body.firstChild
+    )
     .catch((resone) => {
       throw Error(resone);
     });
 }
+// add icons to the DOM
+(function () {
+  // gather all icons containers
+  const all = document.querySelectorAll("[data-icon-wraper]");
+  all.forEach((container) => {
+    // split data come from data-attribute
+    const data = container.dataset.iconWraper;
+
+    fetchIcon(data).then((res) => container.appendChild(res));
+  });
+})();
+
