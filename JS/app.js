e92bc785e071c864ce524ef2354ce163110ddbee
HITO <oumar2000mouhamed@gmail.com>
2022-10-11 15:20:34 +0300

    nothing to mention just some basic javaScript code

--- JS/app.js
+++ JS/app.js
@@ -1,7 +1,10 @@
 const body = document.body;
 const screen = document.getElementById("screen");
 const navLinks = document.querySelectorAll(".nav-link");
+const navLinksActive = document.querySelector(".nav-links [data-active]");
 const searchBar = document.querySelector("[data-search-bar]");
+let iconsUsed = [];
+
 function load_screen(path) {
   if (path == "" || typeof path != "string") return;
   fetch("./" + path)
@@ -13,39 +16,14 @@ function load_screen(path) {
       console.warn(error);
     });
 }
+var a = navLinksActive;
+var b = navLinks[0];
+let p = window.getComputedStyle(b).paddingTop;
 
-// fetch for icons
-function fetchIcon(svgName) {
-  if (svgName === "" || svgName == undefined)
-    return console.error(new Error("pass svg name"));
-  return fetch("imgs/icons/" + svgName + ".svg")
-    .then((res) => res.text())
-    .then((text) => {
-      // remove live-server injection
-      if (text.indexOf("<!-- Code injected by live-server -->") !== -1) {
-        text =
-          text.split("<!-- Code injected by live-server -->")[0] + "</svg>";
-      }
-      return text;
-    })
-    .then(
-      (result) =>
-        // parse an svg from the plain text result
-        new DOMParser().parseFromString(result, "text/html").body.firstChild
-    )
-    .catch((resone) => {
-      throw Error(resone);
-    });
-}
-// add icons to the DOM
-(function () {
-  // gather all icons containers
-  const all = document.querySelectorAll("[data-icon-wraper]");
-  all.forEach((container) => {
-    // split data come from data-attribute
-    const data = container.dataset.iconWraper;
+// a.style.top += `5px`;
 
-    fetchIcon(data).then((res) => container.appendChild(res));
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    // navLinksActive.
   });
-})();
-
+});
