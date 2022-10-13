34766c660aaca3ffd416a715fea06d25930b1d0c
HITO <oumar2000mouhamed@gmail.com>
2022-10-13 18:17:20 +0300

    enhance code a little more

--- JS/app.js
+++ JS/app.js
@@ -3,9 +3,9 @@ const screen = document.getElementById("screen");
 const navLinks = document.querySelectorAll(".nav-link");
 const searchBar = document.querySelector("[data-search-bar]");
 let iconsUsed = [];
-
+load_screen()
 function load_screen(path) {
-  if (path == "" || typeof path != "string") return;
+  if (!path || typeof path != "string") throw Error("specify path for screen");
   fetch("./" + path)
     .then((response) => response.text())
     .then((html) => {
@@ -17,15 +17,13 @@ function load_screen(path) {
 }
 
 (function () {
-  let holder = null;
-
   const active = document.querySelector(".nav-links [data-active]");
   // create an alias for transform:translateY() css property
   Object.defineProperties(active, {
     ty: {
       get() {
-        let style = window.getComputedStyle(this).transform;
-        return new WebKitCSSMatrix(style).m42;
+        let transform = window.getComputedStyle(this).transform;
+        return new WebKitCSSMatrix(transform).m42;
       },
       set(val) {
         return (this.style.transform = `translateY(${val}px)`);
@@ -34,19 +32,19 @@ function load_screen(path) {
   });
 
   navLinks[0].classList.add("active");
-  holder = navLinks[0];
+  let holder = navLinks[0];
+  let calcNextMove = (nextElement) => {
+    let top1 = active.getBoundingClientRect().top,
+      top2 = nextElement.getBoundingClientRect().top;
+    return top2 - top1;
+  };
   // move the active line to the currect active screen
   navLinks.forEach((link) => {
     link.addEventListener("click", () => {
-      active.ty += calculateDistance(link);
+      active.ty += calcNextMove(link);
       holder.classList.remove("active");
       link.classList.add("active");
       holder = link;
     });
   });
-  function calculateDistance(nextElement) {
-    let cury = active.getBoundingClientRect().top;
-    let nexty = nextElement.getBoundingClientRect().top;
-    return nexty - cury;
-  }
 })();
